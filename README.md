0308aebf4d7ae146b7ebb41379f14846d38644a1
Eliane Lell <lelltchatchoua@et.esiea.fr>
2021-12-01 12:15:44 +0100

    Add live badges

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # maven_training
+![example workflow](https://github.com/elianelell/maven_training/actions/workflows/build.yml/badge.svg)
+
+[![codecov](https://codecov.io/gh/elianelell/maven_training/branch/main/graph/badge.svg?token=2RSQSP8UXS)](https://codecov.io/gh/elianelell/maven_training)
+
+
+
+
+
+
+
+
+
+
--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -1,14 +1,45 @@
 package fr.lernejo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 class SampleTest {
 
-    @org.junit.jupiter.api.Test
-    void op() {
+    @Test
+    void add_test() {
+        Sample s = new Sample();
+        Sample.Operation operation = Sample.Operation.ADD;
+        int a = 10;
+        int b = 10;
+        int result = s.op(operation, a, b);
+        Assertions.assertThat(result)
+            .isEqualTo(20);
     }
 
-    @org.junit.jupiter.api.Test
-    void fact() {
+    @Test
+    void mult_test(){
+        Sample s = new Sample();
+        Sample.Operation operation = Sample.Operation.MULT;
+        int a = 2;
+        int b = 3;
+        int result = s.op(operation, a, b);
+        Assertions.assertThat(result)
+            .isEqualTo(6);
+    }
+
+    @Test
+    void test_factorielle() {
+        Sample s = new Sample();
+        int n = 10;
+        int result = s.fact(n);
+        Assertions.assertThat(result).as("fact of ten")
+            .isEqualTo(3628800);
+    }
+
+    @Test
+    void bad_arg() {
+        Sample s = new Sample();
+        int n = -1;
+        org.assertj.core.api.Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> s.fact(n));
     }
 }
